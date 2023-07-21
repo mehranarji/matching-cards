6161450d857fd1cac54b9f127995f137c8ccffb4
mehranarji <mehranarji@gmail.com>
2023-07-22 00:56:30 +0330

    Improve display of matched cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,11 +4,12 @@ import { FC } from "react";
 export interface CardProps {
     children: React.ReactNode;
     isFlipped?: boolean;
+    isCorrect?: boolean;
     className?: string;
     onSelect?: () => void;
 }
 
-const Card: FC<CardProps> = ({ children, className, isFlipped, onSelect }) => {
+const Card: FC<CardProps> = ({ children, className, isFlipped, isCorrect, onSelect }) => {
     return (
         <button
             className={classnames("aspect-square relative select-none cursor-pointer", className)}
@@ -42,6 +43,7 @@ const Card: FC<CardProps> = ({ children, className, isFlipped, onSelect }) => {
                     "p-6",
                     {
                         flip: !isFlipped,
+                        "opacity-40": isCorrect,
                     }
                 )}
             >
